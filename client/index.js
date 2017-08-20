548ec56cd7ceabf9ec7ef2ec88d253b746901945
Kanit Sharma <kanitshrm09@gmail.com>
2017-08-20 19:22:10 +0530

    Added eslint Airbnb-config support

--- client/index.js
+++ client/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable */
 import { Meteor } from 'meteor/meteor';
+/* eslint-enable */
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/app'
+import App from './components/app';
 
 Meteor.startup(() => {
   render(<App />, document.getElementById('app'));
